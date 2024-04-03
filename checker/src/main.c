b26ca0694c62c7f608c11061970c948fa61cb450
Andre Chicu <andriesh.chicu@gmail.com>
2024-04-03 21:56:18 +0300

    batman

--- checker/src/main.c
+++ checker/src/main.c
@@ -1,5 +1,85 @@
 
-#include "zlib.h"
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <zconf.h>
+#include <zlib.h>
+
+typedef struct Buff {
+  char *dat;
+  ulong len;
+} Buff;
+
+Buff *makeBuff(char *input) {
+  Buff *buff = malloc(sizeof(Buff));
+  buff->len = strlen(input) + 1;
+  buff->dat = (char *)malloc(buff->len);
+  strcpy(buff->dat, input);
+  return buff;
+}
+
+Buff *makeZBuff(Buff *inputBuff) {
+  Buff *buff = malloc(sizeof(Buff));
+  // Compress the input data
+  buff->len = compressBound(inputBuff->len);
+  buff->dat = (char *)malloc(buff->len);
+  compress((Bytef *)buff->dat, &buff->len, (const Bytef *)inputBuff->dat,
+           inputBuff->len);
+  return buff;
+}
+Buff *catBuffs(Buff *buff1, Buff *buff2) {
+  Buff *result = malloc(sizeof(Buff));
+  result->len = buff1->len + buff2->len - 1;
+  result->dat = malloc(result->len);
+
+  strcpy(result->dat, buff1->dat);
+  strcat(result->dat, buff2->dat);
+
+  return result;
+}
+
+Buff *maxBuff(Buff *x, Buff *y) {
+  if (x->len > y->len)
+    return x;
+  return y;
+}
+
+Buff *minBuff(Buff *x, Buff *y) {
+  if (x->len < y->len)
+    return x;
+  return y;
+}
+
+float normCompDist(Buff *x, Buff *y) {
+  Buff *Cx = makeZBuff(x);
+  Buff *Cy = makeZBuff(y);
+
+  Buff *xy = catBuffs(x, y);
+  Buff *Cxy = makeZBuff(xy);
+
+  float ncd = (float)((float)Cxy->len - (float)minBuff(Cx, Cy)->len) /
+              (float)maxBuff(Cx, Cy)->len;
+  free(xy->dat);
+  free(xy);
+  return ncd;
+}
+
+int main() {
+  Buff *inBuff_1 =
+      makeBuff("Hello, this is a test string to compress using zlib!");
+  Buff *inBuff_2 =
+      makeBuff("Alien shot Donald in the foot. Now he cries obnoctiously");
+
+  float ncd = normCompDist(inBuff_1, inBuff_2);
+  printf("ncd: %f\n", ncd);
+
+  // Free allocated memory
+  free(inBuff_1->dat);
+  free(inBuff_2->dat);
+  free(inBuff_1);
+  free(inBuff_2);
+
+  return 0;
+}
