0ac0b956770b6c2e51b417632846f66de083af35
IacovlevMaxim <iacovlev.maxim@gmail.com>
2024-04-21 16:20:51 +0300

    Included backend from Vlad

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FileUpload } from 'primereact/fileupload';
 import Verdict from "./components/verdict";
 
 export default function App() {
+  const [ uploaded, setUploaded ] = useState(false);
+  const [ loading, setLoading ] = useState(false);
+
+  useEffect(() => {
+    if(!loading) return;
+
+    setTimeout(() => {
+      setUploaded(true);
+      setLoading(false);
+    }, 5000);
+  }, [loading])
   return (
-    //<div className='w-full h-full w-max-screen h-max-screen bg-gray-50 dark:bg-dark-800'>
-    <Verdict />
-    //</div>
-    // <div className="card">
-    //     <FileUpload 
-    //         name="demo[]" 
-    //         url={'http://192.168.188.76:8000'} 
-    //         onSelect={console.log}
-    //         onUpload={console.log}
-    //         multiple 
-    //         accept=".c,.txt"
-    //         maxFileSize={1000000} 
-    //         emptyTemplate={
-    //             <p className="m-0">Drag and drop files to here to upload.</p>
-    //         } 
-    //     />
-    // </div>
+    loading ? <p className="m-0">Loading...</p> :
+    uploaded
+    ? (
+      <div className='w-full h-full w-max-screen h-max-screen bg-gray-50 dark:bg-dark-800'>
+          <Verdict />
+      </div>
+    ) 
+    : (
+      <div className="card">
+        <FileUpload 
+            name="file" 
+            url={'http://localhost:5000/upload'} 
+            onSelect={() => console.log("Selected")}
+            onUpload={() => setLoading(true)}
+            multiple 
+            accept=".zip"
+            maxFileSize={1000000} 
+            emptyTemplate={
+                <p className="m-0">Drag and drop .zip file with code files</p>
+            } 
+        />
+    </div>
+    )
   )
 }
 
