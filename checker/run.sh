fd4979f3098f916cc58e6e53a0b25f8ca3465004
Andre Chicu <andriesh.chicu@gmail.com>
2024-04-21 16:12:57 +0300

    macos build test

--- checker/run.sh
+++ checker/run.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-gcc ./src/buff.c ./src/parser.c ./src/main.c -lz -lclang -o ./build/main
+
+if [ "$(uname)" = "Linux" ]; then
+  gcc ./src/buff.c ./src/parser.c ./src/main.c -lz -lclang -o ./build/main
+elif [ "$(uname)" = "Darwin" ]; then
+  clang -I./include ./src/buff.c ./src/parser.c ./src/main.c -o ./build/main -lz -L/opt/homebrew/opt/llvm/lib -lclang
+else
+exit 1
+fi
+
+
 ./build/main 
 
 #{
