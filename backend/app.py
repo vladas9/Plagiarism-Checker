806de20b10f6960faf0fe022c151f9303e42f916
Vladislav Amza <130138352+vladas9@users.noreply.github.com>
2024-04-22 10:09:51 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,8 +8,7 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-# Configure the maximum upload size (optional)
-app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024  # 16 MB limit
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024 
 
 
 def run_checker():
@@ -19,28 +18,22 @@ def run_checker():
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
-    # Check if the post request has the file part
     if "file" not in request.files:
         return jsonify({"error": "No file part"}), 400
     file = request.files["file"]
 
-    # If the user does not select a file, the browser submits an
-    # empty file without a filename.
+    
     if file.filename == "":
         return jsonify({"error": "No selected file"}), 400
 
     if file and file.filename.endswith(".zip"):
-        # Saving the file just to unzip it
         filepath = "solutions.zip"
         file.save(filepath)
 
-        # Unzip the file
         try:
             with zipfile.ZipFile(filepath, "r") as zip_ref:
-                # Extract all the contents into directory
                 extract_path = "../checker/solutions"
                 zip_ref.extractall(extract_path)
-                # Delete the .zip file since we no longer need it
                 os.remove(filepath)
                 run_checker()
             return (
